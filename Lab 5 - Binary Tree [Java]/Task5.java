df7cbf299c3eca829176d6a8e51b698da43bb040
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-10 12:10:16 +0600

    Complete subtractSummation method implementation
    Implemented methods to calculate left and right subtree sums and updated subtractSummation to return their difference.

--- Lab 5 - Binary Tree [Java]/Task5.java
+++ Lab 5 - Binary Tree [Java]/Task5.java
@@ -1,7 +1,22 @@
 // Complete the subtractSummation method
 //ASSIGNMENT TASK MUST SUBMIT
 public class Task5 {
+    
+    private Integer sumTree(BTNode root){
+        if(root == null)return 0;
+        return root.elem + sumTree(node.left) + sumTree(node.right);
+    }
 
+    private Integer leftSubtreeSum(BTNode root){
+        if(root == null || root.left == null)return 0;
+        return sumTree(root.left);
+    }
+    
+    private Integer rightSubtreeSum(BTNode root){
+        if(root == null || root.right == null)return 0;
+        return sumTree(root.right);
+    }
+    
     //===================================TASK#5===================================
     // This method takes only 1 parameter which is root of the given tree
     // This method returns an Integer
@@ -9,7 +24,7 @@ public class Task5 {
     // You can use extra helper private static methods with extra extra params as per need
     public static Integer subtractSummation( BTNode root ){
         //TO DO
-        return null; //remove this line
+        return (leftSubtreeSum - rightSubtreeSum); //remove this line
     }
     //============================================================================
 

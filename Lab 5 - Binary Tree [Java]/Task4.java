bad7f1c0f601cee8ac5325d761062f05fe00b3fd
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-11 13:37:53 +0600

    Refactor swapChild method in Task4.java

--- Lab 5 - Binary Tree [Java]/Task4.java
+++ Lab 5 - Binary Tree [Java]/Task4.java
@@ -1,15 +1,6 @@
 // Complete the swapChild method
 //ASSIGNMENT TASK MUST SUBMIT
 public class Task4 {
-    private BTNode newNode(Object elem){ //swap
-        BTNode temp = new BTNode();
-        temp.elem = elem;
-        temp.left = temp.right = null;
-        return temp;
-    }
-    private void swapall(BTNode root, int M){
-        swapChild(root, 1, M);
-    }
     
     //===================================TASK#4===================================
     // This method takes only 3 parameters
@@ -21,13 +12,15 @@ public class Task4 {
     // You can use extra helper private static methods as per need
     public static void swapChild( BTNode root, int lvl, int M ){
         //TO DO
-        if(root == null || root.left == null && root.right == null)return; // base case
-        if((lvl <= M)){
-            Node temp = root.left;
+        if(root == null || root.left == null && root.right ==null) return;
+        if(lvl < M){
+            BTNode temp = root.left;
             root.left = root.right;
             root.right = temp;
         }
-        swapChild(root.left, lvl+1,M);
+        swapChild(root.left, lvl+1, M);
         swapChild(root.right, lvl+1, M);
+
+    }
     //============================================================================
 }

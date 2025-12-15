1a85cd187383efb218ad0e622d156d8ed8e1250b
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-15 11:15:27 +0600

    Update Task6.java

--- Lab_6_Binary_Search_Tree_[Java]/Task6.java
+++ Lab_6_Binary_Search_Tree_[Java]/Task6.java
@@ -1,26 +1,34 @@
 //Assignment Task (must Submit)
-// Complete the isBST method
-public class Task6 {
-    //===================================TASK#6======================
+// Complete the sumOfLeaves method
+
+public class Task5 {
+    static private void inPrint(BSTNode node){
+        if(node == null)return;
+        inPrint(node.left);
+        System.out.println(node.elem);
+        inPrint(node.right);
+
+    }
+   static private void ms(BSTNode nodel, BSTNode noder){
+    if(nodel.left == null && noder.right ==null) return;
+
+    ms(nodel.left, noder.right);
+    System.out.println(nodel.left.elem + " " + noder.right.elem);
+    ms(nodel.right, noder.left);
+   }
+    //===================================TASK#5======================
     // This method takes only one parameter
     // it is root of the given tree
     // You can use extra helper private static methods as per need
-    public static Boolean isBST( BSTNode root ){
+    
+    public static Integer mirrorSum( BSTNode root ){
         //TO DO
-        if(root == null) return false;
-        if(root.right.elem == null || (root.left.elem > root.elem || root.right.elem < root.elem)){
-            return false;
-        }
-        if(root.left.elem< root.elem || root.right.elem > root.elem || (root.left.elem == null && root.elem != null) ){
-            return true;
-        }
-        if(root.left.elem < root.elem || root.left.elem ==null){
-            return isBST(root.left);
-        }if(root.right.elem > root.elem || root.right.elem == null){
-            return isBST(root.right);
-        }
-        return true;
-         // remove this line
+        if(root == null) return 0;        
+        BSTNode rootleft = root.left;        
+        BSTNode rootRight = root.right;
+        ms(rootleft, rootRight);
+        return 0;
+        // remove this line
     }
     //===============================================================
 

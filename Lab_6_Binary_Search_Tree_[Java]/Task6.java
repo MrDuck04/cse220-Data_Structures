dd04482d8a86e8376f903ca55961b70ef5563ec5
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-15 10:17:44 +0600

    Complete isBST method for binary search tree validation
    Implemented the isBST method to check if a binary tree is a binary search tree.

--- Lab_6_Binary_Search_Tree_[Java]/Task6.java
+++ Lab_6_Binary_Search_Tree_[Java]/Task6.java
@@ -1,14 +1,26 @@
 //Assignment Task (must Submit)
 // Complete the isBST method
 public class Task6 {
-
     //===================================TASK#6======================
     // This method takes only one parameter
     // it is root of the given tree
     // You can use extra helper private static methods as per need
     public static Boolean isBST( BSTNode root ){
         //TO DO
-        return null; // remove this line
+        if(root == null) return false;
+        if(root.right.elem == null || (root.left.elem > root.elem || root.right.elem < root.elem)){
+            return false;
+        }
+        if(root.left.elem< root.elem || root.right.elem > root.elem || (root.left.elem == null && root.elem != null) ){
+            return true;
+        }
+        if(root.left.elem < root.elem || root.left.elem ==null){
+            return isBST(root.left);
+        }if(root.right.elem > root.elem || root.right.elem == null){
+            return isBST(root.right);
+        }
+        return true;
+         // remove this line
     }
     //===============================================================
 

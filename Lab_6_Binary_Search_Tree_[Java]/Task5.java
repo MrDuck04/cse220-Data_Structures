192d8b122d607f41391a0ea4d0b12792c504cc6f
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-15 11:17:24 +0600

    Complete mirrorSum method in Task5.java
    Implemented the mirrorSum method to calculate the sum of mirrored leaves.

--- Lab_6_Binary_Search_Tree_[Java]/Task5.java
+++ Lab_6_Binary_Search_Tree_[Java]/Task5.java
@@ -1,14 +1,34 @@
 //Assignment Task (must Submit)
 // Complete the sumOfLeaves method
-public class Task5 {
 
+public class Task5 {
+    static private void inPrint(BSTNode node){
+        if(node == null)return;
+        inPrint(node.left);
+        System.out.println(node.elem);
+        inPrint(node.right);
+
+    }
+   static private void ms(BSTNode nodel, BSTNode noder){
+    if(nodel.left == null && noder.right ==null) return;
+
+    ms(nodel.left, noder.right);
+    System.out.println(nodel.left.elem + " " + noder.right.elem);
+    ms(nodel.right, noder.left);
+   }
     //===================================TASK#5======================
     // This method takes only one parameter
     // it is root of the given tree
     // You can use extra helper private static methods as per need
+    
     public static Integer mirrorSum( BSTNode root ){
         //TO DO
-        return null; // remove this line
+        if(root == null) return 0;        
+        BSTNode rootleft = root.left;        
+        BSTNode rootRight = root.right;
+        ms(rootleft, rootRight);
+        return 0;
+        // remove this line
     }
     //===============================================================
 

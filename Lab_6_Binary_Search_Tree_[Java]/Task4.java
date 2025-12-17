af98f44cf4019b2ee56b3dbfd2b740ffe0eb2af3
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-17 18:14:52 +0600

    Improve rangeSum implementation in Task4.java
    Refactor rangeSum method to use an array and search helper.

--- Lab_6_Binary_Search_Tree_[Java]/Task4.java
+++ Lab_6_Binary_Search_Tree_[Java]/Task4.java
@@ -1,36 +1,71 @@
 //Assignment Task (must Submit)
+// // Complete the rangeSum method
+// public class Task4 {
+
+//     //===================================TASK#4======================
+//     // This method takes only three parameters
+//     // first one is the root of the given tree
+//     // second one the low range and third one is high range
+//     // You can use extra helper private static methods as per need
+//     static int sum = 0;
+//     public static Integer rangeSum( BSTNode root, Integer low, Integer high ){
+//         //TO DO
+//         if( root == null){
+//             return 0;
+//         }
+//         if(root.elem <= high){
+//             sum+=root.elem;
+//             return sum;
+//         }
+//         if(root.elem >= low){
+//             sum+=root.elem;
+//             return sum;
+//         }
+//         if (low < root.elem || high < root.elem) {
+//             sum+=root.elem;
+//             return rangeSum(root.left, low, high);
+//         }
+//         if(low > root.elem || high > root.elem){
+//             sum+=root.elem;
+//             return rangeSum(root.right, low, high);
+//         }
+//         return sum;
+//     //===============================================================
+
+//     }
+// }
+//Assignment Task (must Submit)
 // Complete the rangeSum method
 public class Task4 {
-
+    private static boolean search(BSTNode node, Integer key){
+        if(node == null) return false;
+        if(node.elem == key) return true;
+        if(key < node.elem){
+            return search( node.left, key);
+        }
+        else{
+            return search(node.right, key);
+        }
+    }
     //===================================TASK#4======================
     // This method takes only three parameters
     // first one is the root of the given tree
     // second one the low range and third one is high range
     // You can use extra helper private static methods as per need
-    static int sum = 0;
     public static Integer rangeSum( BSTNode root, Integer low, Integer high ){
         //TO DO
-        if( root == null){
-            return 0;
+        Integer sum = 0;
+        int[] arr = new int[high - low + 1];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = low++;            
+            if(search(root, arr[i])){
+                sum+= arr[i];
+            }
         }
-        if(root.elem <= high){
-            sum+=root.elem;
-            return sum;
-        }
-        if(root.elem >= low){
-            sum+=root.elem;
-            return sum;
-        }
-        if (low < root.elem || high < root.elem) {
-            sum+=root.elem;
-            return rangeSum(root.left, low, high);
-        }
-        if(low > root.elem || high > root.elem){
-            sum+=root.elem;
-            return rangeSum(root.right, low, high);
-        }
-        return sum;
+        
+        return sum; // remove this line
+    }
     //===============================================================
 
-    }
+
 }

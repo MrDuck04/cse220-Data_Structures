e264e4c56246aea197620b5a19560dac0aac52b9
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2025-12-26 21:36:06 +0600

    Update heapCreator.java

--- Lab7/heapCreator.java
+++ Lab7/heapCreator.java
@@ -2,3 +2,45 @@
 //how to create a heap
 //how does a heap work
 //iterate,insert, delete
+public class maxHeap {
+    Integer[] heapArray;
+    Integer capacity;
+    Integer currentSize;
+
+    public maxHeap(Integer capacity){ //1 based array
+        this.capacity = capacity;
+        heapArray = new Integer[capacity];
+        currentSize = 1;
+    }
+
+    public void insert(Integer key){
+        if(currentSize == capacity) return; // full
+        heapArray[currentSize] = key;
+        for(int i = (int)Math.floor(currentSize/2); i >= 1;  i--){
+            maxHeapify(heapArray, i, currentSize);
+        }
+        currentSize++;
+    }
+
+    public void maxHeapify(Integer[] array, Integer p, Integer n){
+        Integer left = 2*p;
+        Integer right = 2*p +1;
+        Integer smallest = p;
+
+        if(left < n && array[left] < array[p]){
+            smallest = left;
+        }
+        if(right < n && array[right] < array[p]){
+            smallest = right;
+        }
+        if(smallest!=p){
+            swap(array, p, smallest);
+            maxHeapify(array, smallest, n);
+        }
+    }
+    public void swap(Integer[] array, Integer a, Integer b){
+        Integer temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
